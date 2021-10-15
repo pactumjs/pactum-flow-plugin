60b0b74594ae3a5705a47082725394f27cd313b5
Anudeep <asa.anudeep4@gmail.com>
2021-10-15 14:33:02 +0530

    removed spec & interaction interface

--- src/reporter.d.ts
+++ src/reporter.d.ts
@@ -1,17 +1,3 @@
-interface Spec {
-  flow: string;
-  request: object;
-  response: object;
-}
-
-interface Interaction {
-  provider: string;
-  flow: string;
-  strict: boolean;
-  request: object;
-  response: object;
-}
-
-export function afterSpec(spec: Spec): void;
-export function afterInteraction(interaction: Interaction): void;
+export function afterSpec(spec: any): void;
+export function afterInteraction(interaction: any): void;
 export function end(): void;
